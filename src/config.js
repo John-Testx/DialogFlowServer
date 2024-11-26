3c9ce752195ca0dce2d766a420d17c7db52fc039
John-Testx <juan.astudillomurua@gmail.com>
2024-11-26 17:19:19 -0300

    v2.011

--- src/config.js
+++ src/config.js
@@ -1,7 +1,12 @@
 import { config } from "dotenv";
 config()
 export const PORT = 3000;
-export const HOST = 'http://localhost:' + PORT;
+const isProduction = process.env.NODE_ENV === 'production';
+
+export const HOST = isProduction
+    ? process.env.HOST // Use production URL
+    : 'http://localhost:' + PORT;
+
 export const PAYPAL_API_CLIENT = process.env.PAYPAL_API_CLIENT;
 export const PAYPAL_API_SECRET = process.env.PAYPAL_API_SECRET;
 export const PAYPAL_API = 'https://api-m.sandbox.paypal.com';
